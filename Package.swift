6adc18c00ce27821506dd7a4aa742b2de90e9334
Alain Meier <me@alainmeier.com>
2026-05-02 13:37:45 -0700

    Release iOS SDK 1.2.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Tripwire",
-            url: "https://github.com/abxy-labs/tripwire-ios/releases/download/1.1.0/Tripwire-1.1.0.xcframework.zip",
-            checksum: "ed9a804181f858c33323755afb94212cbb1e8900c8e95b5981a940b061aa6cee"
+            url: "https://github.com/abxy-labs/tripwire-ios/releases/download/1.2.0/Tripwire-1.2.0.xcframework.zip",
+            checksum: "6a806a9a43dba1b6f34866672c56c6dad6a06284a2bd309acc6d18dc8b5460c0"
         ),
     ]
 )
